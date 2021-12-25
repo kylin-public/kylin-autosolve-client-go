4473c57157a619571c96a19ad0839f07a9259e59
Kylin <60720950+kylinbotteam@users.noreply.github.com>
2021-12-25 10:52:07 +0800

    Rework the locking

--- autosolve_client.go
+++ autosolve_client.go
@@ -31,7 +31,7 @@ var ErrorNotConnected = errors.New("can't make a call when not connected")
 // ErrorLogin is returned when login failed
 var ErrorLogin = errors.New("unable to log in to the server")
 
-// ErrorNoAuthorizationInfo is returned in Start() when required authroization info is missing
+// ErrorNoAuthorizationInfo is returned in Start() but required authroization info is missing
 var ErrorNoAuthorizationInfo = errors.New("authroization info is required")
 
 // RemoteError is the errors returned from server
@@ -58,18 +58,21 @@ type Client struct {
 	HeartBeatTimeout int
 
 	EE            ee.IEventEmitter
+	ee            ee.IEventEmitter
 	nextRequestID uint64
 	requests      sync.Map
 
-	mutex          sync.Mutex
+	connMutex      sync.Mutex
 	cancelFunc     context.CancelFunc
 	retryCount     int
 	loginError     bool
 	ticker         *time.Ticker
 	tickerChan     chan byte
 	heartBeatCount int
-	started        bool
-	loggedIn       bool
+
+	mutex    sync.Mutex
+	started  bool
+	loggedIn bool
 }
 
 // NotificationEvent is the payload object of Notification events
@@ -147,6 +150,7 @@ func New() *Client {
 	c := &Client{
 		ws:               gowebsocket.New(autosolveURL),
 		EE:               ee.NewEventEmitter(),
+		ee:               ee.NewEventEmitter(),
 		HeartBeatTimeout: 8,
 		nextRequestID:    1000,
 	}
@@ -166,20 +170,30 @@ func (c *Client) IsConnected() bool {
 
 // IsLoggedIn returns true if this client connected to server and signed in successfully
 func (c *Client) IsLoggedIn() bool {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
 	return c.loggedIn
 }
 
 // IsStarted returns true if this client has been started
 func (c *Client) IsStarted() bool {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
 	return c.started
 }
 
 // Start connects to kylin autosolve server and signin using provided authorization information
 func (c *Client) Start() error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
 	if c.started {
 		return nil
 	}
 
+	c.retryCount = 0
 	if err := c.connect(); err != nil {
 		return err
 	}
@@ -189,10 +203,23 @@ func (c *Client) Start() error {
 
 // Stop disconnects the underlying websocket connection and abort all pending calls
 func (c *Client) Stop() {
-	if c.started {
-		c.started = false
-		c.close()
+	closing := false
+	(func() {
+		c.mutex.Lock()
+		defer c.mutex.Unlock()
 
+		if c.started {
+			closing = true
+			c.started = false
+
+			// close will emit the Abort event on the internal event emitter to abort all pending
+			// calls, the only listener is installed by the Invoke() and it doesn't take any mutex,
+			// should be safe.
+			c.close()
+		}
+	})()
+
+	if closing {
 		c.EE.Emit("Closed")
 	}
 }
@@ -202,8 +229,12 @@ func (c *Client) WhenReady() error {
 	return c.WhenReadyWithContext(context.Background())
 }
 
-// WhenReadyWithContext blocks current routine until this client is stopped or signin is completed
-func (c *Client) WhenReadyWithContext(ctx context.Context) error {
+var errorNotReady = errors.New("not ready yet")
+
+func (c *Client) whenReady() error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
 	if c.started && c.loggedIn {
 		return nil
 	}
@@ -214,7 +245,15 @@ func (c *Client) WhenReadyWithContext(ctx context.Context) error {
 		return ErrorLogin
 	}
 
-	var result error
+	return errorNotReady
+}
+
+// WhenReadyWithContext blocks current routine until this client is stopped or signin is completed
+func (c *Client) WhenReadyWithContext(ctx context.Context) error {
+	result := c.whenReady()
+	if result != errorNotReady {
+		return result
+	}
 
 	ch := make(chan byte)
 	listener := func(payload ...interface{}) {
@@ -225,32 +264,35 @@ func (c *Client) WhenReadyWithContext(ctx context.Context) error {
 		}
 		ch <- 0
 	}
-	c.EE.Once("Login", listener)
-	c.EE.Once("LoginError", listener)
-	c.EE.Once("Closed", listener)
 
-	select {
-	case <-ch:
-		break
+loop:
+	for {
+		result = nil
+		c.EE.Once("Login", listener)
+		c.EE.Once("LoginError", listener)
+		c.EE.Once("Closed", listener)
 
-	case <-ctx.Done():
+		select {
+		case <-ch:
+
+		case <-ctx.Done():
+			if result == nil {
+				result = ctx.Err()
+			}
+			break loop
+		}
+
+		c.EE.RemoveListener("Login", listener)
+		c.EE.RemoveListener("LoginError", listener)
+		c.EE.RemoveListener("Closed", listener)
+
 		if result == nil {
-			result = ctx.Err()
+			result = c.whenReady()
+			if result == errorNotReady {
+				continue
+			}
 		}
 		break
-	}
-
-	c.EE.RemoveListener("Login", listener)
-	c.EE.RemoveListener("LoginError", listener)
-	c.EE.RemoveListener("Closed", listener)
-
-	if result == nil {
-		if !c.started {
-			return ErrorNotConnected
-		}
-		if c.started && c.loggedIn {
-			return nil
-		}
 	}
 	return result
 }
@@ -416,11 +458,11 @@ func (c *Client) Invoke(context context.Context, message *protocol.Message) (*pr
 			Error: ErrorAborted,
 		}
 	}
-	c.EE.Once("Abort", listener)
+	c.ee.Once("Abort", listener)
 
 	defer (func() {
 		c.requests.Delete(requestID)
-		c.EE.Off("Abort", listener)
+		c.ee.Off("Abort", listener)
 	})()
 	err = c.ws.SendBinary(msg)
 	if err != nil {
@@ -439,7 +481,7 @@ func (c *Client) Invoke(context context.Context, message *protocol.Message) (*pr
 func (c *Client) addEventListeners() {
 	c.ws.OnConnected = func(_ gowebsocket.Socket) {
 		go (func() {
-			if !c.started {
+			if !c.IsStarted() {
 				return
 			}
 			msg := c.MakeLoginMessage()
@@ -447,45 +489,57 @@ func (c *Client) addEventListeners() {
 			if err == nil {
 				login := response.GetResponse().GetLogin()
 				if login.Success {
-					c.startHeartBeatCheck()
-					c.loggedIn = true
-					c.EE.Emit("Login")
+					c.onLoggedIn()
 				} else {
-					c.loginError = true
-					c.EE.Emit("LoginError", ErrorLogin)
+					c.onLoginError()
 				}
 			} else {
-				c.loginError = true
-				c.EE.Emit("LoginError", err)
+				c.onLoginError()
 			}
 		})()
 	}
 	c.ws.OnDisconnected = func(err error, _ gowebsocket.Socket) {
-		c.EE.Emit("Disconnected", err)
-		if c.started {
+		if c.IsStarted() {
+			c.EE.Emit("Disconnected", err)
+
 			go (func() {
+				c.mutex.Lock()
+				defer c.mutex.Unlock()
+
 				if c.started {
 					c._close()
-
 					c.connect()
 				}
 			})()
 		}
 	}
 	c.ws.OnConnectError = func(err error, _ gowebsocket.Socket) {
-		c.EE.Emit("ConnectError", err)
-		if c.started {
-			go (func() {
-				if !c.started {
-					return
-				}
-				c._close()
+		if c.IsStarted() {
+			c.EE.Emit("ConnectError", err)
 
-				c.retryCount++
-				sleepTime := retryIntervals[c.retryCount]
-				time.Sleep(time.Duration(sleepTime) * time.Millisecond)
-				if c.started {
-					c.connect()
+			go (func() {
+				started := false
+				func() {
+					c.mutex.Lock()
+					defer c.mutex.Unlock()
+
+					if c.started {
+						started = true
+						c.retryCount++
+						c._close()
+					}
+				}()
+
+				if started {
+					sleepTime := retryIntervals[c.retryCount%len(retryIntervals)]
+					time.Sleep(time.Duration(sleepTime) * time.Millisecond)
+
+					c.mutex.Lock()
+					defer c.mutex.Unlock()
+
+					if c.started {
+						c.connect()
+					}
 				}
 			})()
 		}
@@ -564,9 +618,52 @@ func (c *Client) onRequest(requestMsg *protocol.Message) {
 	}
 }
 
+func (c *Client) onLoggedIn() {
+
+	started := false
+	(func() {
+		// Takes the c.mutex first
+		c.mutex.Lock()
+		defer c.mutex.Unlock()
+
+		if c.started {
+			started = true
+			c.loggedIn = true
+
+			c.connMutex.Lock()
+			defer c.connMutex.Unlock()
+
+			c.startHeartBeatCheck()
+		}
+	})()
+
+	// Don't emit the login event if this client is already stopped
+	if started {
+		c.EE.Emit("Login")
+	}
+}
+
+func (c *Client) onLoginError() {
+	started := false
+	(func() {
+		c.mutex.Lock()
+		defer c.mutex.Unlock()
+
+		if c.started {
+			started = true
+			c.loginError = true
+		}
+	})()
+
+	// Don't emit the login event if this client is already stopped
+	if started {
+		c.EE.Emit("LoginError", ErrorLogin)
+	}
+}
+
 func (c *Client) connect() error {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
+	c.connMutex.Lock()
+	defer c.connMutex.Unlock()
 
 	if c.AccessToken == "" || c.ClientKey == "" {
 		return ErrorNoAuthorizationInfo
@@ -590,8 +687,8 @@ func (c *Client) connect() error {
 
 func (c *Client) close() {
 	(func() {
-		c.mutex.Lock()
-		defer c.mutex.Unlock()
+		c.connMutex.Lock()
+		defer c.connMutex.Unlock()
 
 		c.removeEventListeners()
 
@@ -604,13 +701,13 @@ func (c *Client) close() {
 		c.stopHeartBeatCheck()
 	})()
 
-	c.EE.Emit("Abort")
+	c.ee.Emit("Abort")
 }
 
 func (c *Client) _close() {
 	(func() {
-		c.mutex.Lock()
-		defer c.mutex.Unlock()
+		c.connMutex.Lock()
+		defer c.connMutex.Unlock()
 
 		c.removeEventListeners()
 		if c.cancelFunc != nil {
@@ -621,7 +718,7 @@ func (c *Client) _close() {
 		c.stopHeartBeatCheck()
 	})()
 
-	c.EE.Emit("Abort")
+	c.ee.Emit("Abort")
 }
 
 func (c *Client) startHeartBeatCheck() {
